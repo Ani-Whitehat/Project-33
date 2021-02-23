eb9d9a547f7c33bdda96ef17e7e159dfbbb3cc27
Ani-Whitehat <76240548+Ani-Whitehat@users.noreply.github.com>
2021-02-23 21:10:05 +0530

    Update Particle.js

--- js/Particle.js
+++ js/Particle.js
@@ -2,12 +2,14 @@ class Particle {
     constructor(x, y,r) {
 
         var options ={
-            restitution:0.4
+            restitution:0.1,
+            density:1.8
         }
-        this.r=r;
+        
         this.x = x;
         this.y = y;
-        this.body = Bodies.circle(x, y, this.r,options);       
+        this.r=r;
+        this.body = Bodies.circle(x, y, r,options);       
         this.color=color(random(0, 255), random(0, 255), random(0, 255));
         World.add(world, this.body);
 
@@ -16,16 +18,15 @@ class Particle {
 
         var pos = this.body.position;
         var angle = this.body.angle;
-
-        push();
-        translate(pos.x, pos.y);
-        rotate(angle);
-        imageMode(CENTER);
-        noStroke();
-        fill(this.color)
-        ellipseMode(CENTER);
-        ellipse(pos.x, pos.y, this.r,this.r);
-        pop();
+            push();
+            translate(pos.x,pos.y);
+            rotate(angle);
+            imageMode(CENTER);
+            noStroke();
+            fill(this.color);
+            ellipseMode(CENTER);
+            ellipse(0, 0, this.r,this.r);
+            pop();      
     }
 
 };
